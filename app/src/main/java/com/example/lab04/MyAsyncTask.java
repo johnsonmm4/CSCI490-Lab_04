56b904418c72fbde09dde0e896f067da569f871d
johnsonmm4 <johnsonmm4@g.cofc.edu>
2020-03-11 18:22:10 -0400

    Init commit

---
+++ app/src/main/java/com/example/lab04/MyAsyncTask.java
@@ -0,0 +1,16 @@
+package com.example.lab04;
+
+import android.os.AsyncTask;
+
+public class MyAsyncTask extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
